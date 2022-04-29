42e25b53b8d4c179ca5aafdfddf4c4e6b6a43717
a-bodrova <soulwent94@gmail.com>
2022-04-29 13:00:03 +0400

    feat: add encode line solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,21 +10,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+
+function encodeLine( str ) {
+  // throw new NotImplementedError('Not implemented');
 
   let res = '';
   let counter = 1;
   
-  for (let i = 0; i < str.length - 1; i++) {
+  for (let i = 1; i < str.length; i++) {
 
-    if (str[i] === str[i + 1]) {
+    if (str[i] === str[i - 1]) {
       counter += 1;
-    } else {
+      if (i === str.length - 1) {
+        res += `${counter}${str[i - 1]}`;
+        break;
+      }
+    }  else {
       if (counter > 1) {
-        res += `${counter}${str[i]}`;
+        res += `${counter}${str[i - 1]}`;
       } else {
-        res += str[i];
+        res += `${str[i - 1]}`;
+      };
+      if (i === str.length - 1) {
+        res += `${str[i]}`;
+        break;
       }
       counter = 1;
     }
