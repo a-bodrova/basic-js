2649ed63d0659cc6e1a9fe4dc9a1a899927e9d7d
a-bodrova <soulwent94@gmail.com>
2022-05-02 23:35:11 +0400

    feat: add some more

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    this.isDirect = direction || true;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.matrix = this.getTable();
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  getFullKey(message, key) {
+    const repeatCount = Math.floor(message.length / key.length);
+    const fullKey = key.repeat(repeatCount);
+    const remainder = key.slice(0, (message.length - fullKey.length));
+    return fullKey + remainder;
+  }
+
+  
+  encrypt(message, key) {
+    // throw new NotImplementedError('Not implemented');
+    if (arguments.length !== 2 || typeof arguments[0] !== 'string' || typeof arguments[1] !== 'string') {
+      throw new Error('Incorrect arguments!');
+    };
+    const mess = message.split(' ').join('');
+    const fullKey = this.getFullKey(mess, key);
+    let encryptedMessage = '';
+
+    for (let i = 0; i < message.length; i++) {
+      const column = this.alphabet.indexOf(mess[i]);
+      const row = this.alphabet.indexOf(fullKey[i]);
+
+      if (column === -1) {
+        encryptedMessage += mess[i];
+      } else {
+        encryptedMessage += this.matrix[row][column];
+      }
+    }
+    
+    return this.isDirect ? encryptedMessage : encryptedMessage.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    // throw new NotImplementedError('Not implemented');
+    if (arguments.length !== 2 || typeof arguments[0] !== 'string' || typeof arguments[1] !== 'string') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const fullKey = this.getFullKey(message, key);
+
+  }
+
+  getTable() {
+    const matrix = [...Array(this.alphabet.length)].map(item => Array(this.alphabet.length).fill(''));
+    for (let i = 0; i < this.alphabet.length; i++) {
+      matrix[i] = this.alphabet.split('');
+      const remainder = matrix[i].splice(0, i);
+      matrix[i] = [...matrix[i], ...remainder];
+    }
+
+    return matrix;
   }
 }
 
